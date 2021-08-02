6930cfd7bbfb48d7066db3ddc46c9e6a46897c8d
yiminghe <yiminghe@gmail.com>
2021-08-02 19:22:09 +0800

    rebuild

--- examples/gen.sh
+++ examples/gen.sh
@@ -1,5 +1,6 @@
 cd xtemplate && sh gen.sh
 cd ../ && cd excel-formula-ll && sh gen.sh
 cd ../ && cd cal-ll && sh gen.sh
+cd ../ && cd regexp-ll && sh gen.sh
 cd ../ && cd cal && sh gen.sh
 cd precedence && sh gen.sh

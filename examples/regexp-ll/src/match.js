c375d1f87296f9873a1b2faf3949830c75d8c9c6
yiminghe <yiminghe@gmail.com>
2021-08-05 18:43:17 +0800

    optimize

--- examples/regexp-ll/src/match.js
+++ examples/regexp-ll/src/match.js
@@ -22,21 +22,25 @@ function matchInput(input, state, callSiteMap = new Map()) {
   }
   let groupEndIndex = this.compiler.groupEndIndex(state);
   if (groupEndIndex) {
-    let startIndex = { ...input.startGroupIndex[groupEndIndex.index - 1] };
+    let startIndex = input.startGroupIndex[groupEndIndex.index - 1];
     let endIndex = {
       index: input.index,
       name: startIndex.name
     };
     if (startIndex.index > endIndex.index) {
+      startIndex = { ...startIndex };
       const s = startIndex.index;
       startIndex.index = Math.max(0, endIndex.index + 1);
       endIndex.index = s + 1;
     }
     let name = groupEndIndex.name;
     let value = {
-      ...startIndex,
+      index: startIndex.index,
       match: input.str.slice(startIndex.index, endIndex.index)
     };
+    if (startIndex.name) {
+      value.name = startIndex.name;
+    }
     input.groups[groupEndIndex.index - 1] = value;
     if (name) {
       input.namedGroups[name] = value;

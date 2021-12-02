ef586bdc44889ccce0b2cc24edfd618454b62423
yiminghe <yiminghe@gmail.com>
2021-12-02 10:22:41 +0800

    vba: update tc

--- examples/vba/tests/pkgTest.ts
+++ examples/vba/tests/pkgTest.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck
 
-import { Context, SubBinder } from '../pkg/';
+import { Context, SubBinding } from '../pkg/';
 
 const sampleCode = `
 sub main
@@ -9,7 +9,7 @@ MsgBox 2
 end sub
 `.trim();
 
-const MsgBoxSub: SubBinder = {
+const MsgBoxSub: SubBinding = {
   name: 'MsgBox',
   argumentsInfo: [
     {
@@ -23,7 +23,7 @@ const MsgBoxSub: SubBinder = {
 
 async function main() {
   const context = new Context();
-  context.registerSubBinder(MsgBoxSub);
+  context.registerSubBinding(MsgBoxSub);
   await context.load(sampleCode);
   await context.callSub('main');
 }
